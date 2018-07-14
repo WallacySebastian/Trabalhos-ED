869e7c1799e8bcb7353ce281599f70dbef2b63c5
WallacySebastian <wallacysebastian@hotmail.com>
2018-07-14 13:02:12 -0300

    reta

---
+++ Geometria/Reta.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include "Reta.h"
+#include "Calculos.h"
+
+struct estrutura_reta {
+	Ponto p1;
+	Ponto p2;
+};
+
+typedef struct estrutura_reta reta;
+
+Reta criarReta(Ponto p1, Ponto p2) {
+	reta *r0 = NULL;
+
+	r0 = (reta *) calloc(1, sizeof(reta));
+
+	r0->p1 = p1;
+	r0->p2 = p2;
+
+	return (Reta) r0;
+}
+
+Ponto getP1(Reta r) {
+	reta *r0 = NULL;
+
+	r0 = (reta *) r;
+
+	return r0->p1;
+}
+
+Ponto getP2(Reta r) {
+	reta *r0 = NULL;
+
+	r0 = (reta *) r;
+
+	return r0->p2;
+}
+
+double tamanhoReta(Reta r) {
+	reta *r0 = NULL;
+	double d = 0;
+
+	r0 = (reta *) r;
+
+	d = distancia(r0->p1, r0->p2);
+
+	return d;
+}
+
+void liberarReta(Reta r) {
+	reta *r0 = NULL;
+
+	r0 = (reta *) r;
+
+	liberarPonto(r0->p1);
+	liberarPonto(r0->p2);
+	free(r0);
+}
