d70efa6f95c0c34f0d39b78b3c083312ddea4aa3
WallacySebastian <wallacysebastian@hotmail.com>
2018-07-14 13:47:14 -0300

    Update Calculos.c

--- Geometria/Calculos.c
+++ Geometria/Calculos.c
@@ -18,6 +18,10 @@ double distancia(Ponto p1, Ponto p2) {
 	return d;
 }
 
+Ponto centroRetangulo(Forma f) {
+	
+} 
+
 int pontoInternoAoCirculo(Ponto p, Objeto obj) {
 	double d = 0;
 	double raio = 0;
