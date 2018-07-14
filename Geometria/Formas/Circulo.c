4c5c652844d0fed7378e8f0437e2180f58c143c6
WallacySebastian <wallacysebastian@hotmail.com>
2018-07-14 13:32:31 -0300

    Create Circulo.c

---
+++ Geometria/Formas/Circulo.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "Circulo.h"
+
+struct estrutura_circulo {
+	double raio;
+};
+
+typedef struct estrutura_circulo circulo;
+
+Circulo criarCirculo(double raio) {
+	circulo *c0 = NULL;
+
+	c0 = (circulo *) calloc(1, sizeof(circulo));
+
+	c0->raio = raio;
+
+	return (Circulo) c0;
+}
+
+double getRaio(Circulo c) {
+	circulo *c0 = NULL;
+
+	c0 = (circulo *) c;
+
+	return c0->raio;
+}
+
+void liberarCirculo(Circulo c) {
+	circulo *c0 = NULL;
+
+	c0 = (circulo *) c;
+
+	free(c0);
+}
